0ed42bafd5b37e85d21136571c8d5e0a16979b0c
James Lesneski <jlesneski@lucidiait.com>
2022-05-22 14:01:24 -0400

    add vm

---
+++ vm1.tf
@@ -0,0 +1,21 @@
+resource "aws_network_interface" "vm1_nic1" {
+  subnet_id   = aws_subnet.vms_subnet.id
+  private_ips = ["10.100.1.71"]
+
+  tags = {
+    Name = lower("vm1-${terraform.workspace}-nic1")
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.vm1_nic1.id
+    device_index         = 0
+  }
+  tags = {
+    Name = lower("vm1-${terraform.workspace}-web1")
+  }
+}
